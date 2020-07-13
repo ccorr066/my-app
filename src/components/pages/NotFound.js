f447d76e0c2d1cac89cf9a26d63853c2735f6412
Christian Corrales <ccorr066@fiu.edu>
2020-07-13 10:28:18 -0700

    need to fix NotFound page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,9 +1,44 @@
 import React from "react"
+import { ReactMic } from "react-mic"
 
 export default function NotFound() {
+  constructor(props)
+  super(props)
+  this.state = {
+    record: false,
+  }
+
+  startRecording = () => {
+    this.setState({ record: true })
+  }
+
+  stopRecording = () => {
+    this.setState({ record: false })
+  }
+
+  onData(recordedBlob)
+  console.log("chunk of real-time data is: ", recordedBlob)
+
+  onStop(recordedBlob)
+  console.log("recordedBlob is: ", recordedBlob)
+
+  render()
   return (
     <>
-      <h1>NotFound</h1>
+      <ReactMic
+        record={this.state.record}
+        className="sound-wave"
+        onStop={this.onStop}
+        onData={this.onData}
+        strokeColor="#000000"
+        backgroundColor="#FF4081"
+      />
+      <button onClick={this.startRecording} type="button">
+        Start
+      </button>
+      <button onClick={this.stopRecording} type="button">
+        Stop
+      </button>
     </>
   )
 }
