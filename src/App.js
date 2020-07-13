4424e9ef9b197ac2443f8dbb1c5d2a76325015a5
Christian Corrales <ccorr066@fiu.edu>
2020-07-13 01:32:15 -0700

    added links to the buttons. changed width on BS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./style/master.scss"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Landing from "./components/pages/Landing"
 import Review from "./components/pages/Review"
+import SearchIt from "./components/pages/SearchIt"
+import MostRecent from "./components/pages/MostRecent"
 import LogInSignUp from "./components/pages/LogInSignUp"
 import NotFound from "./components/pages/NotFound"
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route exact path="/" component={Landing} />
         <Route exact path="/review" component={Review} />
         <Route exact path="/login-signup" component={LogInSignUp} />
+        <Route exact path="/most-recent" component={MostRecent} />
+        <Route exact path="/search-it" component={SearchIt} />
         <Route component={NotFound} />
       </Switch>
     </Router>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,47 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Landing() {
   return (
     <>
-      <h1>Landing</h1>
+      <nav className="navbar navbar-light bg-light">
+        <Link to="/" className="navbar-brand"></Link>
+        <p className="navLink">
+          <Link to="/login-signup">Write A Review</Link>
+        </p>
+        <p className="navLink">
+          <Link to="/login-signup">Sign Up / Login</Link>
+        </p>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="title text-center">
+              Get to Know your future Landlords and their rental properties.
+            </h1>
+            <p className="subTitle text-center">
+              Search your city below to read reviews now!
+            </p>
+            <div className="mt-10">
+              <form className="form-inline">
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Search for your address, zip code, or city "
+                  aria-label="Search"
+                />
+                <Link
+                  to="/search-it"
+                  className="btn btn-outline-success"
+                  type="submit"
+                >
+                  Search
+                </Link>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
