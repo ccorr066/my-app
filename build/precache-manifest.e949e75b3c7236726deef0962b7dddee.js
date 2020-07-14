1b6a13adbe93b8126817f49614da8c26730ad3e5
Christian Corrales <ccorr066@fiu.edu>
2020-07-14 09:46:26 -0700

    bad changes

---
+++ build/precache-manifest.e949e75b3c7236726deef0962b7dddee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd7243aa95bf6a5002238444cff5f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff5ad613c83d89b1ca3",
+    "url": "/static/css/main.ed026f03.chunk.css"
+  },
+  {
+    "revision": "b99a8eac9e1e949e68ac",
+    "url": "/static/js/2.62252f0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.62252f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff5ad613c83d89b1ca3",
+    "url": "/static/js/main.e9c73b49.chunk.js"
+  },
+  {
+    "revision": "c0513da4a070665ac62e",
+    "url": "/static/js/runtime-main.92360f4f.js"
+  },
+  {
+    "revision": "9658ae665eda9f177d535dbac8b6aee2",
+    "url": "/static/media/01 www.101soundboards.com.9658ae66.mp3"
+  },
+  {
+    "revision": "e59ab7fa140e19b3daa653ebb7df2f82",
+    "url": "/static/media/peterGriffin404.e59ab7fa.jpg"
+  }
+]);
