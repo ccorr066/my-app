ad9e941b35e079c302c4787300fd0604a08d84c5
Christian Corrales <ccorr066@fiu.edu>
2020-07-13 17:09:46 -0700

    Updates

--- src/components/pages/CreateReview.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function CreateReview() {
-  return (
-    <>
-      <h1>CR</h1>
-    </>
-  )
-}
--- src/components/pages/MostRecent.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function MostRecent() {
-  return (
-    <>
-      <h1>MR</h1>
-    </>
-  )
-}
